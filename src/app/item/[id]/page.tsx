eb38a45410b7910827ccd2e7915999db81fe167c
arshia <arshiasaberi015@gmail.com>
2026-02-25 22:51:46 +0330

    ccv

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -674,7 +674,7 @@ export default function ProfessionalProductPage() {
     return stars;
   };
 
-  if (loading)
+  if (!comments.length && !user.length && !product?.title && !cart?.items.length )
     return (
       <div className="min-h-screen flex items-center justify-center bg-[#FDFCFB]">
         <div className="flex flex-col items-center gap-3">
