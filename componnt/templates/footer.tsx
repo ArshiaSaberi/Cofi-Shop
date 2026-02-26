4e235fafc624f20411394877027b413d6f859355
arshia <arshiasaberi015@gmail.com>
2026-02-26 22:03:47 +0330

    o

--- componnt/templates/footer.tsx
+++ componnt/templates/footer.tsx
@@ -91,7 +91,7 @@ export default function Footer(){
                       <p className="w-[360.75px] h-[24px] text-[#A1A1A1] text-[15px] leading-[24px] max-md:text-center max-[400px]:w-[230px]">
                         تمام حقوق این سایت متعلق به خانه قهوه چالوس میباشد
                       </p>
-                     <p>Built pixel by pixel by Arshia
+                     <p className=" text-[#A1A1A1] text-[15px] leading-[24px]">Built pixel by pixel by Arshia
 
 </p>
                     </div>
